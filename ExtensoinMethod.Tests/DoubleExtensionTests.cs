10c16020db2ef172483ecbd4dec829956f9ed93a
Denis <denis.balashevich@gmail.com>
2016-07-08 23:56:49 +0300

    Task 3 add!

---
+++ ExtensoinMethod.Tests/DoubleExtensionTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ExtensoinMethod;
+namespace ExtensoinMethod.Tests
+{
+    [TestFixture]
+    public class DoubleExtensionTests
+    {
+        [TestCaseSource("ToIEEE754TestData")]
+        public string Format_Tests(double x)
+        {
+            return DoubleExtension.ToIEEE754(x);
+        }
+        public IEnumerable<TestCaseData> ToIEEE754TestData()
+        {
+            yield return new TestCaseData(123.4).Returns("0000000100111101100110111001100110011001100110011001100101011001");
+            yield return new TestCaseData(0.00000001).Returns("0001111101010001010011110111000101000111000000110011000100010111");
+            yield return new TestCaseData(12365625.265554654).Returns("0100000101110011101010011110011100000101011111110001101101001111");
+            yield return new TestCaseData(0.0).Returns("0000000000000000000000000000000000000000000000000000000000000000");
+        }
+    }
+
+}
---
+++ ExtensoinMethod/DoubleExtension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensoinMethod
+{
+    public static class DoubleExtension
+    {
+        public static string ToIEEE754(this double x)
+        {
+            StringBuilder result = new StringBuilder();
+            byte[] bytes = BitConverter.GetBytes(x);
+
+            foreach (var a in bytes)
+            {
+                result.Append(ToBinary(a));
+            }
+            return Reverse(result.ToString());
+        }
+
+        private static string ToBinary(int x)
+        {
+            string result = Convert.ToString(x, 2);
+            int lenght = result.Length;
+           
+            if (lenght < 8)
+                for (int i = lenght; i < 8; i++)
+                    result += '0';
+            return result;
+        }
+
+        private static string Reverse(string str)
+        {
+            StringBuilder temp = new StringBuilder();
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                temp.Append(str[i]);
+            }
+            return temp.ToString();
+        }
+    }
+}
